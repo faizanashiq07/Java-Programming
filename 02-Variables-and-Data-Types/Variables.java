d9fede8e83abad7f32f3fe725b4bcf0d17fa554d
Faizan Ashiq <faizanashiq07@gmail.com>
2026-09-18 21:05:50 +0530

    Add variables and data types programs

---
+++ 02-Variables-and-Data-Types/Variables.java
@@ -0,0 +1,13 @@
+public class Variables{
+    public static void main(String[] args){
+        int age=5;
+        float mark=60;
+        char grade='A';
+        boolean isStudent=true;
+
+        System.out.println("Age: " + age);
+        System.out.println("Mark: " + mark);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
