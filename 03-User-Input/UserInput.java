8ea1be1d643d40e73ca26369811a787597c9309d
Faizan Ashiq <faizanashiq07@gmail.com>
2026-09-19 23:49:34 +0530

    Add User Input Program

---
+++ 03-User-Input/UserInput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class UserInput {
+    public static void main (String [] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name:");
+        String name=sc.nextLine();
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter your Class:");
+        String classname=sc.nextLine();
+        System.out.println("Your name is: " + name);
+        System.out.println("Your age is: " + age);
+        System.out.println("Your class is: " + classname);
+    }
+}
